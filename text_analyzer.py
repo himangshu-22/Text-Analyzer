af76c8e905c7933f3fb4fbff522d528d0bedbe03
Himangshu Das <yourgithubemail@gmail.com>
2026-09-20 23:18:42 +0530

    Add character count without space

--- text_analyzer.py
+++ text_analyzer.py
@@ -1,2 +1,5 @@
 text = input("enter the text: ")
-print("Number of characters:", len(text))
+print("Total Number of characters:", len(text))
+
+text2= text.replace(" ","")
+print("Number of characters without space:", len(text2))
